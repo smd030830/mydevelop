148a9eb71d3984374739b576ac0f20ba048c0bc1
smd030830 <smd030830@gmail.com>
2026-04-14 18:06:38 +0900

    키오스크 완?

--- cafe_Kiosk/src/main/java/com/mjc813/cafe_kiosk/CafeKioskApplication.java
+++ cafe_Kiosk/src/main/java/com/mjc813/cafe_kiosk/CafeKioskApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CafeKioskApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(CafeKioskApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(CafeKioskApplication.class, args);
+	}
 
 }
--- cafe_Kiosk/src/test/java/com/mjc813/cafe_kiosk/CafeKioskApplicationTests.java
+++ cafe_Kiosk/src/test/java/com/mjc813/cafe_kiosk/CafeKioskApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CafeKioskApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
